c030ed272184df1bf6dd80237b84b0ce3202e092
SanthoshMDS <santhosh.nayak@manipalsystems.com>
2014-08-16 15:49:45 +0530

    first commit

---
+++ Testing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Testing
+//
+//  Created by Santhosh on 16/08/14.
+//  Copyright (c) 2014 com.mds.test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Testing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Testing
+//
+//  Created by Santhosh on 16/08/14.
+//  Copyright (c) 2014 com.mds.test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
